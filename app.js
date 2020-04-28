44df99859d381c15f15bf0da33725fa05c7cd1d6
Jenny <20jennyxu17@gmail.com>
2020-04-28 14:48:26 -0700

    shortened the animation time of landing images

--- app.js
+++ app.js
@@ -1,33 +1,43 @@
-var express       = require("express"),
-    app           = express(),
-    bodyParser    = require("body-parser"),
-    mongoose      = require("mongoose"),
-    flash       = require("connect-flash"),
-    passport      = require("passport"),
-    LocalStrategy = require("passport-local"),
-    methodOverride = require("method-override"),
-    Study_room    = require("./models/study_room"),
-    Comment       = require("./models/comment"),
-    User          = require("./models/user"),
-    seedDB        = require("./seeds");
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  flash = require("connect-flash"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  Study_room = require("./models/study_room"),
+  Comment = require("./models/comment"),
+  User = require("./models/user"),
+  seedDB = require("./seeds");
 
 // requring routes
-var commentRoutes    = require("./routes/comments"),
-    study_roomRoutes = require("./routes/study_rooms"),
-    indexRoutes      = require("./routes/index");
+var commentRoutes = require("./routes/comments"),
+  study_roomRoutes = require("./routes/study_rooms"),
+  indexRoutes = require("./routes/index");
 
-mongoose.connect(process.env.MATLAS, { useUnifiedTopology: true, useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false })
-    .then(() => {
-        console.log('Connected to DB!');
-    }).catch(err => {
-        console.log('ERROR:', err.message);
-});
+mongoose
+  .connect(process.env.MATLAS, {
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Connected to DB!");
+  })
+  .catch((err) => {
+    console.log("ERROR:", err.message);
+  });
 
 // development database
-// mongoose.connect("mongodb://localhost/study_paradise", { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
+// mongoose.connect("mongodb://localhost/study_paradise", {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+// });
 
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
@@ -35,22 +45,24 @@ app.use(flash());
 // seedDB();
 
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "Jenny is the best",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 app.use("/", indexRoutes);
@@ -58,11 +70,11 @@ app.use("/study_rooms", study_roomRoutes);
 app.use("/study_rooms/:id/comments", commentRoutes);
 
 // app.listen(3000, "localhost", function () {
-//     console.log("The study paradise server has started");
+//   console.log("The study paradise server has started");
 // });
 
 app.listen(process.env.PORT, process.env.IP, function () {
-    console.log("The study paradise server has started");
+  console.log("The study paradise server has started");
 });
 
 // console.log(process.env);
